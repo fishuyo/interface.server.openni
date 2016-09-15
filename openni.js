f98106b576a35b1d8677987817b49650a9b2be72
Tim Wood <fishuyo@gmail.com>
2016-09-15 13:35:01 -0700

    working

--- openni.js
+++ openni.js
@@ -3,14 +3,22 @@ var EventEmitter = require( 'events' ).EventEmitter,
 
 OpenNI = {
   inputs: {},
-  outputs:{},
+  outputs: {},
   devices:[],
   getDeviceNames: function() {},
   init: function() {
     if( this.onload ) this.onload()
 
    	this.context = openni();
-    this.context.emit = function(){ OpenNI.emit(arguments); }
+    this.context.emit = function(){ 
+      // console.log(arguments)
+      args = arguments
+      args = Object.keys(args).map( function(k){ return args[k] })
+      event = args[0]
+      // args = if(arguments.length > 2) arguments.slice(1) else arguments
+      self.outputs[event] = {min:0, max:0, value:0}
+      OpenNI.emit(event, args.slice(1));
+    }
     
     OpenNI.devices.push( OpenNI )    
     this.emit( 'new device', 'openni', OpenNI )
